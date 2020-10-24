113aed0c5485a81348ee3d7bfe1e5e93fb8fd745
Mladen Uzelac <uzelac.mladen@gmail.com>
2020-10-24 19:08:58 +0200

    Doesn't print because profile is not set

--- src/main/java/com/uzelac/springit/SpringitApplication.java
+++ src/main/java/com/uzelac/springit/SpringitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.SpringProperties;
 
 @SpringBootApplication
@@ -21,9 +22,10 @@ public class SpringitApplication {
     }
 
     @Bean
+    @Profile("dev")
     CommandLineRunner runner() {
         return args -> {
-            System.out.println("Welcome message: " + springItProperties.getWelcomeMsg());
+            System.out.println("This is something that we would only do in dev");
         };
     }
 }

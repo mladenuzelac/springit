87ecd3929949f1b679468548269dfa663ff2bced
Mladen Uzelac <uzelac.mladen@gmail.com>
2020-10-24 19:37:42 +0200

    Cleaned main file after debugging session

--- src/main/java/com/uzelac/springit/SpringitApplication.java
+++ src/main/java/com/uzelac/springit/SpringitApplication.java
@@ -1,39 +1,17 @@
 package com.uzelac.springit;
 
 import com.uzelac.springit.config.SpringItProperties;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.SpringProperties;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SpringItProperties.class)
 public class SpringitApplication {
 
-    @Autowired
-    private ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner runner() {
-        return args -> {
-            System.out.println("Printing out all the bean names in the application context.");
-            System.out.println("------------------------------------------------------------");
-            String[] beans = applicationContext.getBeanDefinitionNames();
-            Arrays.sort(beans);
-            for (String bean: beans) {
-                System.out.println(bean);
-            }
-        };
-    }
 }

28a15600ca6263324be5ee247b2100c6dba3955a
Mladen Uzelac <uzelac.mladen@gmail.com>
2020-10-25 16:28:05 +0100

    Creating links between entities

--- src/main/java/com/uzelac/springit/model/Comment.java
+++ src/main/java/com/uzelac/springit/model/Comment.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import java.util.Objects;
 
 @Entity
@@ -19,6 +20,7 @@ public class Comment {
     private String body;
 
     // Link
-
+    @ManyToOne
+    private Link link;
 
 }

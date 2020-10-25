2aab66732543a545537ff12d42e502db99179eb7
Mladen Uzelac <uzelac.mladen@gmail.com>
2020-10-25 18:53:52 +0100

    Adding abstract class Auditable and extending in Link and Comment

---
+++ src/main/java/com/uzelac/springit/model/Auditable.java
@@ -0,0 +1,64 @@
+package com.uzelac.springit.model;
+
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    private LocalDateTime creationDate;
+
+    @LastModifiedBy
+    private String lastModifyBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getLastModifyBy() {
+        return lastModifyBy;
+    }
+
+    public void setLastModifyBy(String lastModifyBy) {
+        this.lastModifyBy = lastModifyBy;
+    }
+
+    public LocalDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+
+}
+
--- src/main/java/com/uzelac/springit/model/Link.java
+++ src/main/java/com/uzelac/springit/model/Link.java
@@ -13,9 +13,9 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-@NoArgsConstructor
 @Data
-public class Link {
+@NoArgsConstructor
+public class Link extends Auditable {
 
     @Id
     @GeneratedValue
